84c1dfb95405b241f29adca2fdffb012069ca5bc
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-12-06 00:56:38 +0100

    Add temp project for quick testing

---
+++ RyuSocks.Server/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RyuSocks.Server
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            // Console.WriteLine(RyuSocks.Auth.AuthMethod.TestMe);
+
+            return 0;
+        }
+    }
+}
