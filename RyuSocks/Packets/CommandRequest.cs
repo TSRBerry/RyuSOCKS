4a437791ed82f68469e6194ec828b07ba54d3fe1
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-12-22 06:16:54 +0100

    Move common code to EndpointPacket

--- RyuSocks/Packets/CommandRequest.cs
+++ RyuSocks/Packets/CommandRequest.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Net;
-using System.Text;
 
 namespace RyuSocks.Packets
 {
@@ -31,73 +29,24 @@ namespace RyuSocks.Packets
             Version = array[0];
             Command = (ProxyCommand)array[1];
             Reserved = array[2];
-            AddressType = (AddressType)array[3];
 
-            switch (AddressType)
-            {
-                case AddressType.Ipv4Address:
-                    DestinationAddress = new IPAddress(array[4..8]);
-                    break;
-                case AddressType.DomainName:
-                    DestinationDomainName = Encoding.ASCII.GetString(array, 5, array[4]);
-                    break;
-                case AddressType.Ipv6Address:
-                    DestinationAddress = new IPAddress(array[4..20]);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            DestinationPort = BitConverter.ToUInt16(array, array.Length - 2);
+            base.FromArray(array);
         }
 
         public override byte[] ToArray()
         {
-            // Version + Command + Reserved + AddressType + DestinationPort
-            const int BaseLength = 1 + 1 + 1 + 1 + 2;
-            byte[] array;
+            // Version + Command + Reserved
+            const int BaseLength = 1 + 1 + 1;
+            byte[] endpointArray = base.ToArray();
+            byte[] array = new byte[BaseLength + endpointArray.Length];
 
-            switch (AddressType)
-            {
-                case AddressType.Ipv4Address:
-                {
-                    array = new byte[BaseLength + 4];
+            array[0] = Version;
+            array[1] = (byte)Command;
+            array[2] = Reserved;
 
-                    AssignElements();
-                    DestinationAddress.GetAddressBytes().CopyTo(array, 4);
-                    break;
-                }
-                case AddressType.DomainName:
-                {
-                    array = new byte[BaseLength + DestinationDomainName.Length + 1];
-
-                    AssignElements();
-                    array[4] = (byte)DestinationDomainName.Length;
-                    Encoding.ASCII.GetBytes(DestinationDomainName).CopyTo(array, 5);
-                    break;
-                }
-                case AddressType.Ipv6Address:
-                {
-                    array = new byte[BaseLength + 16];
-
-                    AssignElements();
-                    DestinationAddress.GetAddressBytes().CopyTo(array, 4);
-                    break;
-                }
-                default:
-                    throw new InvalidOperationException();
-            }
+            endpointArray.CopyTo(array, BaseLength);
 
             return array;
-
-            void AssignElements()
-            {
-                array[0] = Version;
-                array[1] = (byte)Command;
-                array[2] = Reserved;
-                array[3] = (byte)AddressType;
-                BitConverter.GetBytes(_port).CopyTo(array, array.Length - 2);
-            }
         }
     }
 }
--- RyuSocks/Packets/CommandResponse.cs
+++ RyuSocks/Packets/CommandResponse.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Net;
-using System.Text;
 
 namespace RyuSocks.Packets
 {
@@ -31,73 +29,24 @@ namespace RyuSocks.Packets
             Version = array[0];
             ReplyField = (ReplyField)array[1];
             Reserved = array[2];
-            AddressType = (AddressType)array[3];
 
-            switch (AddressType)
-            {
-                case AddressType.Ipv4Address:
-                    BoundAddress = new IPAddress(array[4..8]);
-                    break;
-                case AddressType.DomainName:
-                    BoundDomainName = Encoding.ASCII.GetString(array, 5, array[4]);
-                    break;
-                case AddressType.Ipv6Address:
-                    BoundAddress = new IPAddress(array[4..20]);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            BoundPort = BitConverter.ToUInt16(array, array.Length - 2);
+            base.FromArray(array);
         }
 
         public override byte[] ToArray()
         {
-            // Version + ReplyField + Reserved + AddressType + BoundPort
-            const int BaseLength = 1 + 1 + 1 + 1 + 2;
-            byte[] array;
+            // Version + ReplyField + Reserved
+            const int BaseLength = 1 + 1 + 1;
+            byte[] endpointArray = base.ToArray();
+            byte[] array = new byte[BaseLength + endpointArray.Length];
 
-            switch (AddressType)
-            {
-                case AddressType.Ipv4Address:
-                {
-                    array = new byte[BaseLength + 4];
+            array[0] = Version;
+            array[1] = (byte)ReplyField;
+            array[2] = Reserved;
 
-                    AssignElements();
-                    BoundAddress.GetAddressBytes().CopyTo(array, 4);
-                    break;
-                }
-                case AddressType.DomainName:
-                {
-                    array = new byte[BaseLength + BoundDomainName.Length + 1];
-
-                    AssignElements();
-                    array[4] = (byte)BoundDomainName.Length;
-                    Encoding.ASCII.GetBytes(BoundDomainName).CopyTo(array, 5);
-                    break;
-                }
-                case AddressType.Ipv6Address:
-                {
-                    array = new byte[BaseLength + 16];
-
-                    AssignElements();
-                    BoundAddress.GetAddressBytes().CopyTo(array, 4);
-                    break;
-                }
-                default:
-                    throw new InvalidOperationException();
-            }
+            endpointArray.CopyTo(array, BaseLength);
 
             return array;
-
-            void AssignElements()
-            {
-                array[0] = Version;
-                array[1] = (byte)ReplyField;
-                array[2] = Reserved;
-                array[3] = (byte)AddressType;
-                BitConverter.GetBytes(_port).CopyTo(array, array.Length - 2);
-            }
         }
     }
 }
