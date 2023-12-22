d58fce09a222351e39264360d7b8947e3646944b
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-12-22 17:27:02 +0100

    Replace MethodSelectionRequest1 with MethodSelectionRequest

--- RyuSocks/AddressType.cs
+++ RyuSocks/AddressType.cs
@@ -3,7 +3,6 @@ namespace RyuSocks
     public enum AddressType : byte
     {
         Ipv4Address = 0x01,
-        // TODO: Implement support for DomainName and IPv6 addresses to be SOCKS5 compliant
         DomainName = 0x03,
         Ipv6Address,
     }
