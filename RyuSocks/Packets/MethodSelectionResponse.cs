1ad22ffde756959b29423862e17693cef3c0bcac
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-12-22 17:27:16 +0100

    Implement IPacket for MethodSelectionResponse

--- RyuSocks/Packets/MethodSelectionResponse.cs
+++ RyuSocks/Packets/MethodSelectionResponse.cs
@@ -1,10 +1,36 @@
 using RyuSocks.Auth;
+using System;
 
 namespace RyuSocks.Packets
 {
-    public struct MethodSelectionResponse
+    public struct MethodSelectionResponse : IPacket
     {
         public byte Version;
         public AuthMethod Method;
+
+        public void FromArray(byte[] array)
+        {
+            Version = array[0];
+            Method = (AuthMethod)array[1];
+        }
+
+        public byte[] ToArray()
+        {
+            // Version + Method
+            byte[] array = new byte[1 + 1];
+
+            array[0] = Version;
+            array[1] = (byte)Method;
+
+            return array;
+        }
+
+        public void Verify()
+        {
+            if (Version != ProxyConsts.Version)
+            {
+                throw new InvalidOperationException($"Version field is invalid: {Version:X} (Expected: {ProxyConsts.Version:X})");
+            }
+        }
     }
 }
