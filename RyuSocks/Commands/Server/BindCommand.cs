a96545a9d290b70896939aa8a5b8f301fe8fb4e0
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2024-03-26 19:06:26 +0100

    Add bind command implementation

---
+++ RyuSocks/Commands/Server/BindCommand.cs
@@ -0,0 +1,191 @@
+// Copyright (C) RyuSOCKS
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using RyuSocks.Packets;
+using RyuSocks.Types;
+using RyuSocks.Utils;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RyuSocks.Commands.Server
+{
+    [ProxyCommandImpl(0x02)]
+    public partial class BindCommand : ServerCommand, IDisposable
+    {
+        private TcpServer _server;
+        private TcpSession _serverSession;
+
+        public BindCommand(SocksSession session, IPEndPoint boundEndpoint, EndPoint source) : base(session, boundEndpoint, source)
+        {
+            _server = source switch
+            {
+                IPEndPoint ipSource => new TcpServer(this, boundEndpoint, ipSource),
+                DnsEndPoint dnsSource => new TcpServer(this, boundEndpoint, dnsSource),
+                _ => throw new ArgumentException(
+                    "Invalid EndPoint type provided.", nameof(source)),
+            };
+
+            _server.Start();
+        }
+
+        public override void OnReceived(ReadOnlySpan<byte> buffer)
+        {
+            if (_serverSession == null)
+            {
+                throw new InvalidOperationException("No client connected.");
+            }
+
+            if (!_serverSession.IsConnected)
+            {
+                throw new InvalidOperationException("Client disconnected.");
+            }
+
+            _serverSession.SendAsync(buffer);
+        }
+
+        private void SendErrorReply(SocketError error)
+        {
+            Session.SendAsync(new CommandResponse(NullEndPoint)
+            {
+                Version = ProxyConsts.Version,
+                ReplyField = error.ToReplyField(),
+            }.Bytes);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_server != null)
+                {
+                    _server.Stop();
+                    _server.Dispose();
+                    _server = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        class TcpServer : NetCoreServer.TcpServer
+        {
+            private readonly BindCommand _command;
+            private readonly IPEndPoint _sourceEndpoint;
+
+            public TcpServer(BindCommand command, IPEndPoint endpoint, DnsEndPoint source) : base(endpoint)
+            {
+                OptionAcceptorBacklog = 1;
+                _command = command;
+                _sourceEndpoint = new IPEndPoint(Dns.GetHostAddresses(source.Host, source.AddressFamily).First(), source.Port);
+            }
+
+            public TcpServer(BindCommand command, IPEndPoint endpoint, IPEndPoint source) : base(endpoint)
+            {
+                OptionAcceptorBacklog = 1;
+                _command = command;
+                _sourceEndpoint = source;
+            }
+
+            protected override NetCoreServer.TcpSession CreateSession()
+            {
+                return new TcpSession(this, _command);
+            }
+
+            protected override void OnError(SocketError error)
+            {
+                _command.SendErrorReply(error);
+            }
+
+            protected override void OnConnected(NetCoreServer.TcpSession session)
+            {
+                if ((!Equals(_sourceEndpoint, NullEndPoint) && !_sourceEndpoint.Equals(session.Socket.RemoteEndPoint)) || ConnectedSessions > 1)
+                {
+                    session.Disconnect();
+                    return;
+                }
+
+                CommandResponse response = session.Socket.RemoteEndPoint switch
+                {
+                    IPEndPoint ipEndPoint => new CommandResponse(ipEndPoint)
+                    {
+                        Version = ProxyConsts.Version,
+                        ReplyField = ReplyField.Succeeded,
+                    },
+                    DnsEndPoint dnsEndPoint => new CommandResponse(dnsEndPoint)
+                    {
+                        Version = ProxyConsts.Version,
+                        ReplyField = ReplyField.Succeeded,
+                    },
+                    _ => throw new InvalidOperationException(
+                        $"The type of RemoteEndPoint is not supported: {session.Socket.RemoteEndPoint}"),
+                };
+
+                _command.Session.SendAsync(response.Bytes);
+                _command._serverSession = (TcpSession)session;
+            }
+
+            protected override void OnStarted()
+            {
+                CommandResponse response = Endpoint switch
+                {
+                    IPEndPoint ipEndPoint => new CommandResponse(ipEndPoint)
+                    {
+                        Version = ProxyConsts.Version,
+                        ReplyField = ReplyField.Succeeded,
+                    },
+                    DnsEndPoint dnsEndPoint => new CommandResponse(dnsEndPoint)
+                    {
+                        Version = ProxyConsts.Version,
+                        ReplyField = ReplyField.Succeeded,
+                    },
+                    _ => throw new InvalidOperationException(
+                        $"The type of EndPoint is not supported: {Endpoint}"),
+                };
+
+                _command.Session.SendAsync(response.Bytes);
+            }
+        }
+
+        class TcpSession : NetCoreServer.TcpSession
+        {
+            private readonly BindCommand _command;
+
+            public TcpSession(NetCoreServer.TcpServer server, BindCommand command) : base(server)
+            {
+                _command = command;
+            }
+
+            protected override void OnError(SocketError error)
+            {
+                _command.SendErrorReply(error);
+            }
+
+            protected override void OnConnected()
+            {
+                ReceiveAsync();
+            }
+
+            protected override void OnReceived(byte[] buffer, long offset, long size)
+            {
+                _command.Session.SendAsync(buffer.AsSpan((int)offset, (int)size));
+            }
+        }
+    }
+}
