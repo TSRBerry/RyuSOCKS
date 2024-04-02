7e5a7643e26c26c7b25776062036ec82b34bc42d
Niklaas Wriedt <wriedt.niklaas@gmail.com>
2024-04-02 16:18:55 +0200

    run dotnet because someone hasn't automated it yet

--- RyuSocks/Packets/Auth/UsernameAndPassword/UsernameAndPasswordRequest.cs
+++ RyuSocks/Packets/Auth/UsernameAndPassword/UsernameAndPasswordRequest.cs
@@ -90,11 +90,11 @@ namespace RyuSocks.Packets.Auth.UsernameAndPassword
         public UsernameAndPasswordRequest(byte[] packetBytes)
         {
             Bytes = packetBytes;
-            Version = array[0];
-            UsernameLength = array[1];
-            Username = Encoding.ASCII.GetString(array[2..(2 + UsernameLength)]);
-            PasswordLength = array[2 + UsernameLength];
-            Password = Encoding.ASCII.GetString(array[(2 + UsernameLength + 1)..((2 + UsernameLength + 1) + PasswordLength)]);
+            Version = Bytes[0];
+            UsernameLength = Bytes[1];
+            Username = Encoding.ASCII.GetString(Bytes[2..(2 + UsernameLength)]);
+            PasswordLength = Bytes[2 + UsernameLength];
+            Password = Encoding.ASCII.GetString(Bytes[(2 + UsernameLength + 1)..((2 + UsernameLength + 1) + PasswordLength)]);
         }
 
         public UsernameAndPasswordRequest(string username, string password)
@@ -126,7 +126,7 @@ namespace RyuSocks.Packets.Auth.UsernameAndPassword
                 throw new InvalidOperationException($"$Username is invalid: {Username} (Expected: Literally Anything but null)");
             }
 
-            if(Password == null)
+            if (Password == null)
             {
                 throw new InvalidOperationException($"Password is invalid: {Password} (No Password is not allowed)");
             }
