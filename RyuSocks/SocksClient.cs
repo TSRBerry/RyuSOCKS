7a66bb4778cf79f072dc766e451ae90dc145a28f
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2024-05-04 00:08:48 +0200

    Add WIP SocksClient implementation

--- RyuSocks/SocksClient.cs
+++ RyuSocks/SocksClient.cs
@@ -15,6 +15,17 @@
  */
 
 using NetCoreServer;
+using RyuSocks.Auth;
+using RyuSocks.Auth.Extensions;
+using RyuSocks.Commands;
+using RyuSocks.Packets;
+using RyuSocks.Types;
+using RyuSocks.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace RyuSocks
 {
@@ -24,5 +35,108 @@ namespace RyuSocks
         // TODO: Add (generated) properties for auth methods and commands
         // TODO: Keep track of the connection state
         // TODO: Expose events and methods to work with this client
+
+        protected bool IsAuthenticated;
+        protected bool IsCommandAccepted;
+        protected ClientCommand Command;
+        protected IProxyAuth Auth;
+
+        public IReadOnlyDictionary<AuthMethod, IProxyAuth> OfferedAuthMethods { get; set; } = new Dictionary<AuthMethod, IProxyAuth>();
+
+        protected override void OnConnected()
+        {
+            var request = new MethodSelectionRequest(OfferedAuthMethods.Keys.ToArray())
+            {
+                Version = ProxyConsts.Version,
+            };
+
+            SendAsync(request.AsSpan());
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            ReadOnlySpan<byte> bufferSpan = buffer.AsSpan((int)offset, (int)size);
+
+            if (Auth == null)
+            {
+                ProcessAuthMethodSelection(bufferSpan);
+                IsAuthenticated = Auth?.GetAuth() == AuthMethod.NoAuth;
+
+                return;
+            }
+
+            bufferSpan = Auth.Unwrap(bufferSpan);
+
+            if (Command == null)
+            {
+                ProcessCommandResponse(bufferSpan);
+                return;
+            }
+
+            bufferSpan = Command.Unwrap(bufferSpan);
+
+            OnReceived(bufferSpan);
+        }
+
+        protected virtual void OnReceived(ReadOnlySpan<byte> buffer) { }
+
+        protected virtual void ProcessAuthMethodSelection(ReadOnlySpan<byte> buffer)
+        {
+            var response = new MethodSelectionResponse(buffer.ToArray());
+            response.Validate();
+
+            Debug.Assert(OfferedAuthMethods.ContainsKey(response.Method));
+            Auth = OfferedAuthMethods[response.Method];
+        }
+
+        protected virtual void ProcessCommandResponse(ReadOnlySpan<byte> buffer)
+        {
+            var response = new CommandResponse(buffer.ToArray());
+            response.Validate();
+
+            EnsureSuccessReply(response.ReplyField);
+            IsCommandAccepted = true;
+
+            // TODO: Notify command about destination from response
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected static void EnsureSuccessReply(ReplyField replyField)
+        {
+            if (replyField != ReplyField.Succeeded)
+            {
+                throw new ProxyException(replyField);
+            }
+        }
+
+        public override bool SendAsync(ReadOnlySpan<byte> buffer)
+        {
+            if (Command != null && IsCommandAccepted)
+            {
+                buffer = Command.Wrap(buffer);
+            }
+
+            if (IsAuthenticated)
+            {
+                buffer = Auth.Wrap(buffer);
+            }
+
+            return base.SendAsync(buffer);
+        }
+
+        public override long Send(ReadOnlySpan<byte> buffer)
+        {
+            if (Command != null && IsCommandAccepted)
+            {
+                buffer = Command.Wrap(buffer);
+            }
+
+            if (IsAuthenticated)
+            {
+                buffer = Auth.Wrap(buffer);
+            }
+
+            return base.Send(buffer);
+        }
     }
 }
